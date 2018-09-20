a2318b6054c98fa645ff21e0f4de258478108229
Von Arzu <vonarzu@gmail.com>
2018-09-20 16:55:17 -0400

    Basic Movement, direct, randomy
    Coded the C# script in appletree to do the movements randomly

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class AppleTree : MonoBehaviour {
     [Header("Set in Inspector")]
     // prefab for instantiating apples
-    public GameObject  applePrefab;
+    public GameObject applePrefab;
 
     //Speed at which the AppleTree moves
     public float speed = 1f;
@@ -15,19 +15,40 @@ public class AppleTree : MonoBehaviour {
 
     //Chance that the AppleTree will change directions
     public float chanceToChangeDirections = 0.1f;
-   
+
     //Rate at which Apples will be instantiated
     public float secondsBetweenAppleDrops = 1f;
 
-   void Start () {
+    void Start() {
         //Dropping apples every second
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
         //Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
         //Changing Direction
-		
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); // Move Right
+
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); // Move Left
+
+        }                                                   //a
 	}
+    void FixedUpdate() {
+        //Changing Direction Randomly is now time-based because of FixedUpdate()
+        if(Random.value < chanceToChangeDirections){        //b
+            speed *= -1; //Change direction
+
+        }
+        
+    }
 }
