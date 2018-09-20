3144c67f91bc44702a96fcfd2fe035438c1c098a
Von Arzu <vonarzu@gmail.com>
2018-09-20 13:04:23 -0400

    Camera Setup & GamePanel Settings
    learned perspective & orthogonal camera and edited the c# AppleTree

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AppleTree : MonoBehaviour {
+    [Header("Set in Inspector")]
+    // prefab for instantiating apples
+    public GameObject  applePrefab;
 
-	// Use this for initialization
-	void Start () {
+    //Speed at which the AppleTree moves
+    public float speed = 1f;
+
+    //Distance where AppleTree turns around
+    public float leftAndRightEdge = 10f;
+
+    //Chance that the AppleTree will change directions
+    public float chanceToChangeDirections = 0.1f;
+   
+    //Rate at which Apples will be instantiated
+    public float secondsBetweenAppleDrops = 1f;
+
+   void Start () {
+        //Dropping apples every second
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        //Basic Movement
+        //Changing Direction
 		
 	}
 }
